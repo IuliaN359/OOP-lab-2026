a5cba132c32062fe947f8363fd938e5b7d18be34
IuliaN359 <iulianan3333@gmail.com>
2026-02-25 19:30:23 +0200

    Initial commit

---
+++ src/lab1.cpp
@@ -0,0 +1,10 @@
+//
+// Created by iulin on 2/25/2026.
+//
+#include <iostream>
+//using namespace std;
+int main() {
+    const unsigned char endl = '\n';
+    std::cout << "Hello World" << endl;
+    return 0;
+}
